1f0e2f281f058fa3c49c93dc59cd73c6d6093fba
The1nst1gator <tculhane1@gmail.com>
2024-07-24 19:38:15 -0400

    added ability to get book details by isbn and author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,5 +1,6 @@
 const express = require('express');
 let books = require("./booksdb.js");
+const { restart } = require('nodemon');
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
@@ -15,19 +16,37 @@ public_users.get('/',function (req, res) {
   // Fetch all books, convert to JSON string and send string as a response
   const booksJSON = JSON.stringify(books); 
   res.send(booksJSON); 
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: "No Books Found"});
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = parseInt(req.params.isbn, 10);
+    const book = books[isbn];
+    
+    if (book) {
+        res.json(book);
+    } else {
+        res.status(300).json({ error: 'Book not found' });
+    }
+});
   
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book details by author
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author.toLowerCase();
+    const bookDetails = [];
+
+    for (const key in books) {
+        if (books[key].author.toLowerCase() === author) {
+            bookDetails.push(books[key]);
+        }
+    }
+
+    if (bookDetails.length > 0) {
+        res.json(bookDetails);
+    } else {
+        res.status(404).json({ error: 'No books found by this author' });
+    }
 });
 
 // Get all books based on title
